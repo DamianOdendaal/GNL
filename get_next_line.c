d083fd7023e048879aebd416e8d7080233bff717
Damian ODENDAAL <dodendaa@c4r19s4.wethinkcode.co.za>
2019-07-29 16:57:20 +0200

    lol

--- get_next_line.c
+++ get_next_line.c
@@ -3,42 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: event <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: dodendaa <dodendaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/26 11:38:45 by event             #+#    #+#             */
-/*   Updated: 2019/07/26 14:41:11 by event            ###   ########.fr       */
+/*   Updated: 2019/07/29 16:56:36 by dodendaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		buff_read(char **line, char *buff)
+int		buff_read(int fd, char **buff)
 {
-	int i;
-	int start;
+	int		bytes_read;
+	char	*store;
+	char	temp[BUFF_SIZE + 1];
 
-	i = 0;
-	start = 0;
-	while(buff[i] != '\n' || buff[i] != '\0')
-		i++;
-	if(buff[i] == '\n')// if we find the \n char make the
+	if (!(*buff))
 	{
-		line = ft_strsub(buff, start, i);
+		*buff = ft_strnew(0);
 	}
-		
+
+	bytes_read = read(fd, temp, BUFF_SIZE + 1);
+	if (bytes_read <= 0)
+	{
+		return (0);
+	}
+	temp[bytes_read] = '\0';
+	
+
+	// int i;
+	// int start;
+	// int rd;
+
+	// i = 0;
+	// start = 0;
+	// while(buff[i] != '\n' || buff[i] != '\0')
+	// 	i++;
 }
 	/* after reading from the buffer we need to store the information somewhere*/
 
-void	info_move(); // first learn how to move information that you have read !!
+void	info_move();     // first learn how to move information that you have read !!
 
 int		get_next_line(const int fd, char **line)
 {
-	static char *stat[1024];// crart
+	static char *stat[4096];
 	char *buff;
 
 	if (fd < 0 || read(fd, NULL, 0) || !line)
 		return(-1);
-	/*read from the buffer here */
-	then do error check for buffer read here 
+	buff = ft_strnew(BUFF_SIZE);
+	if (!stat[fd])
+		return(-1);
+	
+	/*read from the buffer here 
+	then do error check for buffer read here*/
+
+
 
 }
