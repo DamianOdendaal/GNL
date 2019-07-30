7d3d37bfccd97b8e7c956c95de7cf9501e0f0eea
Damian ODENDAAL <dodendaa@c4r24s1.wethinkcode.co.za>
2019-07-30 15:24:51 +0200

    work added

--- get_next_line.c
+++ get_next_line.c
@@ -17,32 +17,18 @@ int		buff_read(int fd, char **buff)
 	int		bytes_read;
 	char	*store;
 	char	temp[BUFF_SIZE + 1];
-
-	if (!(*buff))
+	while(*buff != '\n')
 	{
-		*buff = ft_strnew(0);
+		if (!(*buff))
+			*buff = ft_strnew(0);
+		bytes_read = read(fd, temp, BUFF_SIZE);
+		if (bytes_read <= 0)
+			return (0);
+		temp[bytes_read] = '\0';
+		store = ft_strjoin(*buff, temp);
+		free(buff);
+
 	}
-
-	bytes_read = read(fd, temp, BUFF_SIZE + 1);
-	if (bytes_read <= 0)
-	{
-		return (0);
-	}
-	temp[bytes_read] = '\0';
-	
-
-	// int i;
-	// int start;
-	// int rd;
-
-	// i = 0;
-	// start = 0;
-	// while(buff[i] != '\n' || buff[i] != '\0')
-	// 	i++;
-}
-	/* after reading from the buffer we need to store the information somewhere*/
-
-void	info_move();     // first learn how to move information that you have read !!
 
 int		get_next_line(const int fd, char **line)
 {
@@ -52,8 +38,6 @@ int		get_next_line(const int fd, char **line)
 	if (fd < 0 || read(fd, NULL, 0) || !line)
 		return(-1);
 	buff = ft_strnew(BUFF_SIZE);
-	if (!stat[fd])
-		return(-1);
 	
 	/*read from the buffer here 
 	then do error check for buffer read here*/
